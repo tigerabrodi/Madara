caad670fac15ea41c60748182619412c94e7de0e
tigerabrodi <tigerabrodi@gmail.com>
2021-02-09 07:39:11 +0100

    feat: setup and add deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -20,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

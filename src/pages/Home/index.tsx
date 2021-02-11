6ea5f1c068ef5792eef13eef6ef2baf794759a58
tigerabrodi <tigerabrodi@gmail.com>
2021-02-11 08:32:57 +0100

    feat(Home): add more core styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,67 @@
 import * as React from 'react'
-import { Form, HomeMain, Subtitle, Title } from './styles'
+import {
+  ErrorMessage,
+  Form,
+  FormGroup,
+  FormTitle,
+  HomeMain,
+  Input,
+  Label,
+  SubmitButton,
+  Subtitle,
+  SwitchButton,
+  Title,
+  SwitchButtonHighlight,
+} from './styles'
 export const Home = () => (
   <HomeMain>
     <Title>Madara</Title>
     <Subtitle>Manage Your Daily Tasks</Subtitle>
-    <Form />
+    <Form>
+      <FormTitle>Sign Up</FormTitle>
+
+      <FormGroup role="group" aria-labelledby="name">
+        <Label htmlFor="name" id="name">
+          Name
+        </Label>
+        <Input type="text" id="name" />
+        <ErrorMessage
+          role="alert"
+          aria-label="Name must be at least two characters long."
+        >
+          Name must be at least two characters long.
+        </ErrorMessage>
+      </FormGroup>
+
+      <FormGroup role="group" aria-labelledby="email">
+        <Label htmlFor="email" id="email">
+          Email
+        </Label>
+        <Input type="email" id="email" />
+        <ErrorMessage role="alert" aria-label="Email is not valid.">
+          Email is not valid.
+        </ErrorMessage>
+      </FormGroup>
+
+      <FormGroup role="group" aria-labelledby="password">
+        <Label htmlFor="password" id="password">
+          Password
+        </Label>
+        <Input type="password" id="password" />
+        <ErrorMessage
+          role="alert"
+          aria-label="Password must be at least 6 characters long."
+        >
+          Password must be at least 6 characters long.
+        </ErrorMessage>
+      </FormGroup>
+
+      <SubmitButton type="submit">Sign Up</SubmitButton>
+
+      <SwitchButton type="button">
+        Already have an account?{' '}
+        <SwitchButtonHighlight>Sign In!</SwitchButtonHighlight>
+      </SwitchButton>
+    </Form>
   </HomeMain>
 )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -11,7 +11,8 @@ export const HomeMain = styled.main`
     'subtitle'
     'form';
   justify-items: center;
-  grid-template-rows: 15% 10% 75%;
+  align-items: center;
+  grid-template-rows: 15% 7% 78%;
 `
 
 export const Title = styled.h1`
@@ -27,13 +28,87 @@ export const Subtitle = styled.h2`
   font-size: 3.5rem;
   font-weight: 600;
   color: ${theme.Blue};
+  text-decoration: underline;
 `
 
 export const Form = styled.form`
   grid-area: form;
   background-color: ${theme.Blue};
   border-radius: 0.2rem;
-  height: 80%;
-  width: 65%;
-  max-width: 85rem;
+  box-shadow: 0 0 1rem var(--blue);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 95%;
+  width: 85rem;
+`
+
+export const FormTitle = styled.h1`
+  font-family: ${theme.LibreBaskerville};
+  color: ${theme.White};
+  font-size: 4rem;
+`
+
+export const FormGroup = styled.div`
+  height: 13rem;
+  width: 60%;
+  display: grid;
+  grid-template-areas:
+    'label'
+    'input'
+    'errorMessage';
+  justify-items: center;
+`
+
+export const Label = styled.label`
+  grid-area: label;
+  color: ${theme.White};
+  font-weight: bold;
+  font-size: 2rem;
+  font-family: ${theme.SourceSansPro};
+  justify-self: start;
+  align-self: center;
+`
+
+export const Input = styled.input`
+  border: none;
+  border-radius: 0.2rem;
+  grid-area: input;
+  background-color: ${theme.White};
+  width: 100%;
+  height: 100%;
+`
+
+export const ErrorMessage = styled.span`
+  font-family: ${theme.SourceSansPro};
+  font-weight: bold;
+  font-size: 1.7rem;
+  grid-area: errorMessage;
+  color: ${theme.Pink};
+  align-self: center;
+`
+
+export const SubmitButton = styled.button`
+  font-family: ${theme.SourceSansPro};
+  background-color: ${theme.White};
+  color: ${theme.Blue};
+  border: none;
+  width: 13rem;
+  height: 4.5rem;
+  font-weight: bold;
+  font-size: 2rem;
+`
+
+export const SwitchButton = styled.button`
+  text-decoration: underline;
+  color: ${theme.White};
+  font-family: ${theme.SourceSansPro};
+  font-weight: bold;
+  background-color: transparent;
+  border: none;
+`
+
+export const SwitchButtonHighlight = styled.span`
+  font-size: 1.5rem;
 `

a5b70ae24ec9c766e6a832193ba9d2b5189971d7
tigerabrodi <tigerabrodi@gmail.com>
2021-02-11 08:55:30 +0100

    feat(Home): add login mode state and simple onSubmit function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,55 +13,74 @@ import {
   Title,
   SwitchButtonHighlight,
 } from './styles'
-export const Home = () => (
-  <HomeMain>
-    <Title>Madara</Title>
-    <Subtitle>Manage Your Daily Tasks</Subtitle>
-    <Form>
-      <FormTitle>Sign Up</FormTitle>
+export const Home = () => {
+  const [isLoginMode, setIsLoginMode] = React.useState(false)
 
-      <FormGroup role="group" aria-labelledby="name">
-        <Label htmlFor="name" id="name">
-          Name
-        </Label>
-        <Input type="text" id="name" />
-        <ErrorMessage
-          role="alert"
-          aria-label="Name must be at least two characters long."
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+  }
+
+  return (
+    <HomeMain>
+      <Title>Madara</Title>
+      <Subtitle>Manage Your Daily Tasks</Subtitle>
+      <Form onSubmit={(event) => onSubmit(event)} autoComplete="off">
+        <FormTitle> {isLoginMode ? 'Sign In' : 'Sign Up'} </FormTitle>
+
+        {!isLoginMode && (
+          <FormGroup role="group" aria-labelledby="name">
+            <Label htmlFor="name" id="name">
+              Name
+            </Label>
+            <Input type="text" id="name" />
+            <ErrorMessage
+              role="alert"
+              aria-label="Name must be at least two characters long."
+            >
+              Name must be at least two characters long.
+            </ErrorMessage>
+          </FormGroup>
+        )}
+
+        <FormGroup role="group" aria-labelledby="email">
+          <Label htmlFor="email" id="email">
+            Email
+          </Label>
+          <Input type="email" id="email" />
+          <ErrorMessage role="alert" aria-label="Email is not valid.">
+            Email is not valid.
+          </ErrorMessage>
+        </FormGroup>
+
+        <FormGroup role="group" aria-labelledby="password">
+          <Label htmlFor="password" id="password">
+            Password
+          </Label>
+          <Input type="password" id="password" />
+          <ErrorMessage
+            role="alert"
+            aria-label="Password must be at least 6 characters long."
+          >
+            Password must be at least 6 characters long.
+          </ErrorMessage>
+        </FormGroup>
+
+        <SubmitButton type="submit">
+          {isLoginMode ? 'Sign In' : 'Sign Up'}
+        </SubmitButton>
+
+        <SwitchButton
+          type="button"
+          onClick={() => setIsLoginMode(!isLoginMode)}
         >
-          Name must be at least two characters long.
-        </ErrorMessage>
-      </FormGroup>
-
-      <FormGroup role="group" aria-labelledby="email">
-        <Label htmlFor="email" id="email">
-          Email
-        </Label>
-        <Input type="email" id="email" />
-        <ErrorMessage role="alert" aria-label="Email is not valid.">
-          Email is not valid.
-        </ErrorMessage>
-      </FormGroup>
-
-      <FormGroup role="group" aria-labelledby="password">
-        <Label htmlFor="password" id="password">
-          Password
-        </Label>
-        <Input type="password" id="password" />
-        <ErrorMessage
-          role="alert"
-          aria-label="Password must be at least 6 characters long."
-        >
-          Password must be at least 6 characters long.
-        </ErrorMessage>
-      </FormGroup>
-
-      <SubmitButton type="submit">Sign Up</SubmitButton>
-
-      <SwitchButton type="button">
-        Already have an account?{' '}
-        <SwitchButtonHighlight>Sign In!</SwitchButtonHighlight>
-      </SwitchButton>
-    </Form>
-  </HomeMain>
-)
+          {isLoginMode
+            ? 'Do not have an account yet?'
+            : 'Already have an account?'}{' '}
+          <SwitchButtonHighlight>
+            {isLoginMode ? 'Sign Up' : 'Sign In!'}
+          </SwitchButtonHighlight>
+        </SwitchButton>
+      </Form>
+    </HomeMain>
+  )
+}

d5514d8ad0ee819f9a7374a07137f20d6fcec400
tigerabrodi <tigerabrodi@gmail.com>
2021-02-13 15:16:42 +0100

    feat(Home): implement form validation logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,64 +15,124 @@ import {
 } from './styles'
 export const Home = () => {
   const [isLoginMode, setIsLoginMode] = React.useState(false)
+  const [isNameError, setIsNameError] = React.useState(false)
+  const [isPasswordError, setIsPasswordError] = React.useState(false)
+  const [isEmailError, setIsEmailError] = React.useState(false)
+
+  const [isEmailInvalid, setIsEmailInvalid] = React.useState(false)
 
   const onSubmit = (event: React.FormEvent) => {
     event.preventDefault()
+
+    type EventWithElements = typeof event.target & {
+      elements: {
+        name: {
+          value: string
+        }
+        email: {
+          value: string
+        }
+        password: {
+          value: string
+        }
+      }
+    }
+
+    const target = event.target as EventWithElements
+
+    const { name, password } = target.elements
+
+    const isNameInvalid = name.value.length < 2
+    if (isNameInvalid) {
+      setIsNameError(true)
+      return setTimeout(() => {
+        setIsNameError(false)
+      }, 2500)
+    }
+
+    if (isEmailInvalid) {
+      setIsEmailError(true)
+      return setTimeout(() => {
+        setIsEmailError(false)
+      }, 2500)
+    }
+
+    const isPasswordInvalid = password.value.length < 6
+    if (isPasswordInvalid) {
+      setIsPasswordError(true)
+      return setTimeout(() => {
+        setIsPasswordError(false)
+      }, 2500)
+    }
   }
 
   return (
     <HomeMain>
       <Title>Madara</Title>
       <Subtitle>Manage Your Daily Tasks</Subtitle>
-      <Form onSubmit={(event) => onSubmit(event)} autoComplete="off">
+      <Form onSubmit={(event) => onSubmit(event)} autoComplete="off" noValidate>
         <FormTitle> {isLoginMode ? 'Sign In' : 'Sign Up'} </FormTitle>
 
         {!isLoginMode && (
-          <FormGroup role="group" aria-labelledby="name">
-            <Label htmlFor="name" id="name">
-              Name
-            </Label>
-            <Input type="text" id="name" aria-describedby="nameInputError" />
-            <ErrorMessage
-              role="alert"
-              id="nameInputError"
-              aria-label="Name must be at least two characters long."
-            >
-              Name must be at least two characters long.
-            </ErrorMessage>
+          <FormGroup role="group">
+            <Label htmlFor="name">Name</Label>
+            <Input
+              type="text"
+              id="name"
+              aria-describedby={isNameError ? 'nameInputError' : undefined}
+            />
+            {isNameError && (
+              <ErrorMessage
+                role="alert"
+                id="nameInputError"
+                aria-label="Name must be at least two characters long."
+              >
+                Name must be at least two characters long.
+              </ErrorMessage>
+            )}
           </FormGroup>
         )}
 
-        <FormGroup role="group" aria-labelledby="email">
-          <Label htmlFor="email" id="email">
-            Email
-          </Label>
-          <Input type="email" id="email" aria-describedby="emailInputError" />
-          <ErrorMessage
-            role="alert"
-            aria-label="Email is not valid."
-            id="emailInputError"
-          >
-            Email is not valid.
-          </ErrorMessage>
+        <FormGroup role="group">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            aria-describedby={isEmailError ? 'emailInputError' : undefined}
+            required
+            onChange={(event) => {
+              setIsEmailInvalid(!event.target.validity.valid)
+            }}
+          />
+          {isEmailError && (
+            <ErrorMessage
+              role="alert"
+              aria-label="Email is not valid."
+              id="emailInputError"
+            >
+              Email is not valid.
+            </ErrorMessage>
+          )}
         </FormGroup>
 
-        <FormGroup role="group" aria-labelledby="password">
-          <Label htmlFor="password" id="password">
-            Password
-          </Label>
+        <FormGroup role="group">
+          <Label htmlFor="password">Password</Label>
           <Input
             type="password"
             id="password"
-            aria-describedby="passwordInputError"
+            aria-describedby={
+              isPasswordError ? 'passwordInputError' : undefined
+            }
           />
-          <ErrorMessage
-            role="alert"
-            aria-label="Password must be at least 6 characters long."
-            id="passwordInputError"
-          >
-            Password must be at least 6 characters long.
-          </ErrorMessage>
+          {isPasswordError && (
+            <ErrorMessage
+              role="alert"
+              aria-label="Password must be at least 6 characters long."
+              id="passwordInputError"
+            >
+              Password must be at least 6 characters long.
+            </ErrorMessage>
+          )}
         </FormGroup>
 
         <SubmitButton type="submit">

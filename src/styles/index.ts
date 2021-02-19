4418e97b5e3dfd6891be709821a7f3d2e6886957
tigerabrodi <tigerabrodi@gmail.com>
2021-02-19 07:12:54 +0100

    feat(styles): update focus styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,12 +1,9 @@
 import { css } from 'styled-components'
-import { theme } from 'theme/theme'
 
 export const focusStyles = css`
   &:focus-visible {
-    outline: 0.2rem solid ${theme.Black};
-    border: 0.2rem solid ${theme.LightBlue};
-    border-radius: 0.2rem;
-    padding: 0.3rem;
+    outline: 0.3rem solid darkorange;
+    outline-offset: 0.1rem;
   }
   &:focus:not(:focus-visible) {
     outline: none;

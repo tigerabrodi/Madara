3a1a0bf074217de646b7484696166db38d215e78
tigerabrodi <tigerabrodi@gmail.com>
2021-02-24 08:37:43 +0100

    feat(Alert): add component and make responsive

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,3 +1,19 @@
-import { AlertWrapper } from './styles'
+import {
+  AlertClose,
+  AlertCloseButton,
+  AlertIcon,
+  AlertMessage,
+  AlertStatus,
+  AlertWrapper,
+} from './styles'
 
-export const Alert = () => <AlertWrapper />
+export const Alert = () => (
+  <AlertWrapper role="alert" tabIndex={0}>
+    <AlertIcon title="check" />
+    <AlertStatus>Success!</AlertStatus>
+    <AlertCloseButton aria-label="Close toast">
+      <AlertClose aria-hidden="true" />
+    </AlertCloseButton>
+    <AlertMessage>You have successfully logged in.</AlertMessage>
+  </AlertWrapper>
+)
--- src/components/Alert/styles.ts
+++ src/components/Alert/styles.ts
@@ -1,12 +1,111 @@
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
+import { theme } from 'theme/theme'
+import { ReactComponent as CheckSVG } from 'assets/check.svg'
+import { ReactComponent as AlertCloseSVG } from 'assets/close.svg'
+import { focusStyles } from 'styles'
+import { media } from 'theme/media'
 
 export const AlertWrapper = styled.div`
   position: absolute;
-  top: 25%;
+  top: 10%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 80vw;
-  max-width: 68rem;
-  min-width: 62rem;
-  height: 11.2rem;
+  width: 90vw;
+  max-width: 85rem;
+  height: 10rem;
+  min-width: 31rem;
+  border: 0.2rem solid ${theme.DarkGreen};
+  background-color: ${theme.Green};
+  box-shadow: 0 0.1rem 0.5rem ${theme.DarkGreen};
+  display: grid;
+  grid-template-columns: 13% 27% 20% 30% 10%;
+  grid-template-areas:
+    'icon status status status close'
+    '. message message message .';
+  align-items: center;
+  justify-items: center;
+  ${focusStyles};
+  ${media.phone} {
+    grid-template-columns: 25% 15% 20% 30% 10%;
+    top: 11%;
+  }
+  ${media.tablet} {
+    grid-template-columns: 25% 15% 20% 20% 20%;
+    min-width: 60rem;
+    width: 80vw;
+    height: 13rem;
+  }
+`
+
+export const AlertStatus = styled.h1`
+  grid-area: status;
+  justify-self: flex-start;
+  padding-left: 1rem;
+  font-family: ${theme.SourceSansPro};
+  font-weight: bold;
+  font-size: 2rem;
+  color: ${theme.DarkGreen};
+  ${media.tablet} {
+    padding-left: 1.8rem;
+    font-size: 2.6rem;
+  }
+`
+
+export const AlertMessage = styled.p`
+  grid-area: message;
+  justify-self: flex-start;
+  align-self: flex-start;
+  padding-left: 1.2rem;
+  font-family: ${theme.SourceSansPro};
+  font-weight: 400;
+  font-size: 1.6rem;
+  color: ${theme.DarkGreen};
+  ${media.tablet} {
+    padding-left: 2rem;
+    font-size: 2rem;
+  }
+`
+
+export const AlertCloseButton = styled.button`
+  grid-area: close;
+  justify-self: flex-end;
+  margin-right: 2rem;
+  height: 1.6rem;
+  width: 1.6rem;
+  border: none;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  ${focusStyles};
+  ${media.phone} {
+    margin-right: 3rem;
+  }
+  ${media.tablet} {
+    height: 2rem;
+    width: 2rem;
+  }
+`
+
+export const AlertClose = styled(AlertCloseSVG)`
+  height: 100%;
+  width: 100%;
+  path {
+    fill: ${theme.DarkGreen};
+  }
+`
+
+export const AlertIcon = styled(CheckSVG)`
+  grid-area: icon;
+  justify-self: end;
+  height: 1.8rem;
+  width: 1.8rem;
+  ${media.tablet} {
+    height: 2rem;
+    width: 2rem;
+  }
+  ${media.tablet} {
+    height: 2.4rem;
+    width: 2.4rem;
+  }
 `

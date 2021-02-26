f6207084c174111b134dbe8dc75773747140b043
tigerabrodi <tigerabrodi@gmail.com>
2021-02-26 08:02:24 +0100

    test: use fake timers in setup in tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,3 +2,12 @@ import '@testing-library/jest-dom/extend-expect'
 import { configure } from 'test-utils'
 
 configure({ defaultHidden: true })
+
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
+afterEach(() => {
+  jest.runOnlyPendingTimers()
+  jest.useRealTimers()
+})

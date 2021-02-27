a792c0cf1a3e9e5a7ab56acb7fd99f8eb7b1b09f
tigerabrodi <tigerabrodi@gmail.com>
2021-02-27 08:39:02 +0100

    feat(ConfirmationModal): use useClickOutside hook with ref inside component :metal:

--- src/components/ConfirmationModal/index.tsx
+++ src/components/ConfirmationModal/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { ModalOverlay } from 'styles'
+import { useClickOutside } from 'hooks/useClickOutside'
 import {
   ConfirmationModal as Modal,
   ConfirmButton,
@@ -9,30 +10,34 @@ import {
 } from './styles'
 
 type ConfirmationModalProps = {
-  ref: React.RefObject<HTMLDivElement>
+  setOpen: (state: boolean) => void
   toggleModal: () => void
   text: string
 }
 
 export const ConfirmationModal = ({
-  ref,
+  setOpen,
   toggleModal,
   text,
-}: ConfirmationModalProps) => (
-  <>
-    <Modal role="alertdialog" aria-modal="true" tabIndex={0} ref={ref}>
-      <ConfirmationTitle>Are you sure?</ConfirmationTitle>
-      <ConfirmationText>{text}</ConfirmationText>
-      <ConfirmButton>Yes</ConfirmButton>
-      <CancelButton
-        onClick={(event) => {
-          event.stopPropagation()
-          toggleModal()
-        }}
-      >
-        No
-      </CancelButton>
-    </Modal>
-    <ModalOverlay aria-hidden="true" />
-  </>
-)
+}: ConfirmationModalProps) => {
+  const [ref] = useClickOutside(() => setOpen(false))
+
+  return (
+    <>
+      <Modal role="alertdialog" aria-modal="true" tabIndex={0} ref={ref}>
+        <ConfirmationTitle>Are you sure?</ConfirmationTitle>
+        <ConfirmationText>{text}</ConfirmationText>
+        <ConfirmButton>Yes</ConfirmButton>
+        <CancelButton
+          onClick={(event) => {
+            event.stopPropagation()
+            toggleModal()
+          }}
+        >
+          No
+        </CancelButton>
+      </Modal>
+      <ModalOverlay aria-hidden="true" />
+    </>
+  )
+}

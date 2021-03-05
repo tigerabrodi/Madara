5f73e9b5f80b1787e33ede02f3e54c5f192dd102
tigerabrodi <tigerabrodi@gmail.com>
2021-03-05 07:39:31 +0100

    feat: add logic for accessible modals

--- src/components/ConfirmationModal/index.tsx
+++ src/components/ConfirmationModal/index.tsx
@@ -22,6 +22,40 @@ export const ConfirmationModal = ({
 }: ConfirmationModalProps) => {
   const [ref] = useClickOutside(() => setOpen(false))
 
+  React.useEffect(() => {
+    const focusableElements = ref.current?.querySelectorAll(
+      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]'
+    )
+
+    const firstItem = focusableElements![0] as HTMLElement
+    const lastItem = focusableElements![
+      focusableElements!.length - 1
+    ] as HTMLElement
+
+    firstItem.focus()
+
+    const trapTabKey = (event: KeyboardEvent) => {
+      if (event.key === 'Tab') {
+        if (event.shiftKey) {
+          if (document.activeElement === firstItem) {
+            event.preventDefault()
+            lastItem.focus()
+          }
+        } else {
+          if (document.activeElement === lastItem) {
+            event.preventDefault()
+            firstItem.focus()
+          }
+        }
+      } else if (event.key === 'Escape') {
+        setOpen(false)
+      }
+    }
+
+    ref.current?.addEventListener('keydown', trapTabKey)
+    return () => document.removeEventListener('keydown', trapTabKey)
+  }, [])
+
   return (
     <>
       <Modal role="alertdialog" aria-modal="true" tabIndex={0} ref={ref}>

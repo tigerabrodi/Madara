8c7ffb9183ca37bbebade24efa2abe257d0f72e0
tigerabrodi <tigerabrodi@gmail.com>
2021-03-05 19:34:04 +0100

    feat(EditModal): add a way to handle the success path via prop

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -108,6 +108,7 @@ export const Board = () => {
       {isEditFormOpen && (
         <EditModal
           setOpen={setIsEditFormOpen}
+          onSuccess={() => 'Success handling edit modal'}
           toggleModal={toggleEditModalForm}
           taskText="Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />

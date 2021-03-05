113842283e674011498c9a8ad58340adcc60d467
tigerabrodi <tigerabrodi@gmail.com>
2021-03-05 07:49:02 +0100

    feat(hooks): add useTrapTabKey hook

---
+++ src/hooks/useTrapTabKey.ts
@@ -0,0 +1,46 @@
+import * as React from 'react'
+
+type TrapTabKeyParams = {
+  ref: React.RefObject<HTMLElement>
+  setOpen: (state: boolean) => void
+  pause?: boolean
+}
+
+export const useTrapTabKey = ({ ref, setOpen, pause }: TrapTabKeyParams) => {
+  React.useEffect(() => {
+    if (pause) {
+      return
+    }
+    const focusableElements = ref.current?.querySelectorAll(
+      'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]'
+    )
+
+    const firstItem = focusableElements![0] as HTMLElement
+    const lastItem = focusableElements![
+      focusableElements!.length - 1
+    ] as HTMLElement
+
+    firstItem.focus()
+
+    const trapTabKey = (event: KeyboardEvent) => {
+      if (event.key === 'Tab') {
+        if (event.shiftKey) {
+          if (document.activeElement === firstItem) {
+            event.preventDefault()
+            lastItem.focus()
+          }
+        } else {
+          if (document.activeElement === lastItem) {
+            event.preventDefault()
+            firstItem.focus()
+          }
+        }
+      } else if (event.key === 'Escape') {
+        setOpen(false)
+      }
+    }
+
+    ref.current?.addEventListener('keydown', trapTabKey)
+    return () => document.removeEventListener('keydown', trapTabKey)
+  }, [pause])
+}

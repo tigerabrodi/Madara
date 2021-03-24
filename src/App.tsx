a488a3c3b6ea3f877669aca84085cc788f13c84b
tigerabrodi <tigerabrodi@gmail.com>
2021-03-24 06:53:04 +0100

    feat: initialize firebase for local development

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
 import { Route, Switch } from 'react-router-dom'
 import { Footer } from 'components/Footer'
 import { AppProviders } from 'context'
@@ -6,24 +12,33 @@ import { Board } from 'pages/Board'
 import { Alert } from 'components/Alert'
 import { Navigation } from 'components/Navigation'
 
-/* TODO 
-   Get user.
-   If no user return Home.
-   If user return Board
-*/
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_API_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_APP_ID,
+  measurementId: process.env.REACT_APP_API_MEASUREMENT_ID,
+})
 
-const App = () => (
-  <AppProviders>
-    <>
-      <Alert />
-      <Navigation />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/board" component={Board} />
-      </Switch>
-      <Footer />
-    </>
-  </AppProviders>
-)
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
+const App = () => {
+  return (
+    <AppProviders>
+      <>
+        <Alert />
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/board" component={Board} />
+        </Switch>
+        <Footer />
+      </>
+    </AppProviders>
+  )
+}
 
 export default App

735d79da3ca6029d5d568cfab438ba81029e5413
tigerabrodi <tigerabrodi@gmail.com>
2021-03-28 09:23:32 +0200

    feat: add type for task

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,9 @@
 export type ColumnType = 'Todo' | 'In progress' | 'Done'
+
+export type TaskType = {
+  text: string
+  createdAt: string
+  userId: string
+  id: string
+  columnType: ColumnType
+}

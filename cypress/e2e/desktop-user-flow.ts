c14d2b0342709db35d46fadad28f028d611ee392
tigerabrodi <tigerabrodi@gmail.com>
2021-03-29 06:14:13 +0200

    test: add flow for deleting a task and logging out

--- cypress/e2e/desktop-user-flow.ts
+++ cypress/e2e/desktop-user-flow.ts
@@ -20,33 +20,51 @@ context('Desktop resolution', () => {
     cy.findByLabelText('Enter Your Name').type(user.name)
     cy.findByLabelText('Enter Your Email').type(user.email)
     cy.findByLabelText('Enter Your Password').type(user.password)
-
     cy.findByRole('button', { name: 'Sign Up' }).click()
 
-    cy.findByText('Signing Up').should('exist')
-
     cy.findByRole('heading', { name: 'Success!' }).should('exist')
-
     cy.findByText('You have successfully signed up.').should('exist')
-
-    cy.findByRole('button', { name: 'Logout' }).should('exist')
-
     cy.findByRole('heading', {
       name: `Welcome ${user.name}!`,
       level: 1,
     }).should('exist')
 
     cy.findByRole('region', { name: 'Todo column with 0 tasks' }).within(() => {
+      cy.findByText('Todo').should('exist')
       cy.findByRole('button', { name: 'Add a task to this column.' }).click()
-
       cy.findByRole('textbox', { name: 'Enter a task' }).type(todoTask.text)
-
       cy.findByRole('button', { name: 'Add' }).click()
-
       cy.findByRole('article', { name: 'Task in Todo column' }).within(() => {
         cy.findByText(todoTask.text).should('exist')
         cy.findByRole('button', { name: 'Card menu' }).should('exist')
       })
     })
+
+    cy.findByRole('region', { name: 'Todo column with 1 tasks' }).within(() => {
+      cy.findByRole('article', { name: 'Task in Todo column' }).within(() => {
+        cy.findByRole('button', { name: 'Card menu' }).click()
+        cy.findByRole('menu').within(() => {
+          cy.findByRole('menuitem', { name: 'Delete Task' }).click()
+          cy.findByRole('alertdialog').within(() => {
+            cy.findByRole('heading', { name: 'Are you sure?' }).should('exist')
+            cy.findByText(
+              'Do you really want to delete this task in Todo column?'
+            ).should('exist')
+            cy.findByRole('button', { name: 'Yes' }).click()
+          })
+        })
+      })
+    })
+
+    cy.findByRole('region', { name: 'Todo column with 0 tasks' }).within(() => {
+      cy.findByRole('article').should('not.exist')
+    })
+
+    cy.findByRole('button', { name: 'Logout' }).click()
+    cy.findByLabelText('Enter Your Name').type(user.name)
+    cy.findByLabelText('Enter Your Email').type(user.email)
+    cy.findByLabelText('Enter Your Password').type(user.password)
+    cy.findByRole('button', { name: 'Sign Up' }).click()
+    cy.findByRole('alert', { name: 'Email is already taken.' }).should('exist')
   })
 })

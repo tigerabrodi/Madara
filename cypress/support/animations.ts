b72e613745e9d0293a16848d621863cac4b5f6a8
tigerabrodi <tigerabrodi@gmail.com>
2021-04-29 14:18:11 +0200

    test: drag and drop reorder tasks within section

---
+++ cypress/support/animations.ts
@@ -0,0 +1,8 @@
+export const timings = {
+  outOfTheWay: 0.2,
+  // greater than the out of the way time
+  // so that when the drop ends everything will
+  // have to be out of the way
+  minDropTime: 0.33,
+  maxDropTime: 0.55,
+}

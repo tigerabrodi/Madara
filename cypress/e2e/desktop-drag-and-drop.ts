e7499b395dbe60aba5014147c62778f247f83d75
tigerabrodi <tigerabrodi@gmail.com>
2021-04-30 18:11:41 +0200

    tests: reorder tasks between sections
    The test has been refactored and also improved.
    
    I previously tried focusing on the paragraph, which obvsiously doesn't work xD.
    
    We now assert that the task's text is within the columns, since it is just a single task in the columns anyways.

--- cypress/e2e/desktop-drag-and-drop.ts
+++ cypress/e2e/desktop-drag-and-drop.ts
@@ -8,11 +8,11 @@ context('Desktop drag and drop functionality', () => {
     indexedDB.deleteDatabase('firebaseLocalStorageDb')
   })
 
-  const todoTask = buildTask()
-  const secondTodoTask = buildTask()
-
   it('Should reorder tasks within a section', () => {
     const user = buildUser()
+
+    const todoTask = buildTask()
+    const secondTodoTask = buildTask()
 
     cy.visit('/')
 
@@ -76,6 +76,9 @@ context('Desktop drag and drop functionality', () => {
   it('should reorder tasks between sections', () => {
     const user = buildUser()
 
+    const firstTask = buildTask()
+    const secondTask = buildTask()
+
     cy.visit('/')
 
     cy.findByLabelText('Enter Your Name').type(user.name, { force: true })
@@ -102,18 +105,15 @@ context('Desktop drag and drop functionality', () => {
     cy.findByRole('region', { name: 'Todo column with 0 tasks' }).within(() => {
       cy.findByText('Todo').should('exist')
       cy.findByRole('button', { name: 'Add a task to this column.' }).click()
-      cy.findByRole('textbox', { name: 'Enter a task' }).type(todoTask.text, {
+      cy.findByRole('textbox', { name: 'Enter a task' }).type(firstTask.text, {
         force: true,
       })
       cy.findByRole('button', { name: 'Add' }).click()
 
       cy.findByRole('button', { name: 'Add a task to this column.' }).click()
-      cy.findByRole('textbox', { name: 'Enter a task' }).type(
-        secondTodoTask.text,
-        {
-          force: true,
-        }
-      )
+      cy.findByRole('textbox', { name: 'Enter a task' }).type(secondTask.text, {
+        force: true,
+      })
       cy.findByRole('button', { name: 'Add' }).click()
     })
 
@@ -132,50 +132,43 @@ context('Desktop drag and drop functionality', () => {
     cy.findByRole('region', {
       name: 'In progress column with 1 tasks',
     }).within(() => {
-      cy.findByRole('button', { name: 'Task in In progress column' }).within(
-        () => {
-          cy.findByText(secondTodoTask.text)
-            .focus()
-            .trigger('keydown', { keyCode: keyCodes.space })
-            .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
-            .wait(timings.outOfTheWay * 1000)
-            .trigger('keydown', { keyCode: keyCodes.space, force: true })
-        }
-      )
+      cy.findByText(secondTask.text).should('exist')
+      cy.findByRole('button', { name: 'Task in In progress column' })
+        .focus()
+        .trigger('keydown', { keyCode: keyCodes.space })
+        .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+        .wait(timings.outOfTheWay * 1000)
+        .trigger('keydown', { keyCode: keyCodes.space, force: true })
     })
 
     cy.findByRole('region', {
       name: 'Done column with 1 tasks',
     }).within(() => {
-      cy.findByRole('button', { name: 'Task in Done column' }).within(() => {
-        cy.findByText(secondTodoTask.text)
-          .focus()
-          .trigger('keydown', { keyCode: keyCodes.space })
-          .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
-          .wait(timings.outOfTheWay * 1000)
-          .trigger('keydown', { keyCode: keyCodes.space, force: true })
-      })
+      cy.findByText(secondTask.text).should('exist')
+      cy.findByRole('button', { name: 'Task in Done column' })
+        .focus()
+        .trigger('keydown', { keyCode: keyCodes.space })
+        .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
+        .wait(timings.outOfTheWay * 1000)
+        .trigger('keydown', { keyCode: keyCodes.space, force: true })
     })
 
     cy.findByRole('region', {
       name: 'In progress column with 1 tasks',
     }).within(() => {
-      cy.findByRole('button', { name: 'Task in In progress column' }).within(
-        () => {
-          cy.findByText(secondTodoTask.text)
-            .focus()
-            .trigger('keydown', { keyCode: keyCodes.space })
-            .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
-            .wait(timings.outOfTheWay * 1000)
-            .trigger('keydown', { keyCode: keyCodes.space, force: true })
-        }
-      )
+      cy.findByText(secondTask.text).should('exist')
+      cy.findByRole('button', { name: 'Task in In progress column' })
+        .focus()
+        .trigger('keydown', { keyCode: keyCodes.space })
+        .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
+        .wait(timings.outOfTheWay * 1000)
+        .trigger('keydown', { keyCode: keyCodes.space, force: true })
     })
 
     cy.findByRole('region', {
       name: 'Todo column with 2 tasks',
     }).within(() => {
-      cy.findAllByRole('button', { name: 'Task in In progress column' }).should(
+      cy.findAllByRole('button', { name: 'Task in Todo column' }).should(
         'have.length',
         2
       )

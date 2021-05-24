78ccc18f873dbddd3bc077edbfe7e4c8f6867a2f
tigerabrodi <tigerabrodi@gmail.com>
2021-05-24 14:36:18 +0200

    feat(Alert): focus on the alert wrapper after alert has been shown
    This way the user can easily close the alert afterwards.

--- src/components/Alert/AlertItem.tsx
+++ src/components/Alert/AlertItem.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { Alert } from './AlertStore'
 import {
   AlertClose,
@@ -16,21 +17,34 @@ type AlertItemProps = {
 export const AlertItem = ({
   alert: { id, message, type },
   removeAlert,
-}: AlertItemProps) => (
-  <AlertWrapper role="alert" tabIndex={0}>
-    <AlertIcon title="check" aria-hidden="true" />
-    <AlertStatus>
-      {type === 'success'
-        ? 'Success!'
-        : type === 'error'
-        ? 'Error!'
-        : type === 'warning'
-        ? 'Warning!'
-        : null}
-    </AlertStatus>
-    <AlertCloseButton aria-label="Close alert" onClick={() => removeAlert(id)}>
-      <AlertClose aria-hidden="true" />
-    </AlertCloseButton>
-    <AlertMessage>{message}</AlertMessage>
-  </AlertWrapper>
-)
+}: AlertItemProps) => {
+  const alertWrapperRef = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    if (message && alertWrapperRef.current) {
+      alertWrapperRef.current.focus()
+    }
+  }, [message])
+
+  return (
+    <AlertWrapper role="alert" tabIndex={-1} ref={alertWrapperRef}>
+      <AlertIcon title="check" aria-hidden="true" />
+      <AlertStatus>
+        {type === 'success'
+          ? 'Success!'
+          : type === 'error'
+          ? 'Error!'
+          : type === 'warning'
+          ? 'Warning!'
+          : null}
+      </AlertStatus>
+      <AlertCloseButton
+        aria-label="Close alert"
+        onClick={() => removeAlert(id)}
+      >
+        <AlertClose aria-hidden="true" />
+      </AlertCloseButton>
+      <AlertMessage>{message}</AlertMessage>
+    </AlertWrapper>
+  )
+}

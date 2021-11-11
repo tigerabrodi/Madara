953f06b7a4d3395b65ab06dafa09b81b472889f3
tigerabrodi <tigerabrodi@gmail.com>
2021-11-11 07:08:54 +0100

    chore(context): change to proper types

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react'
 import { GlobalStyle } from '../theme/globalStyles'
 
-export const AppProviders = ({
-  children,
-}: {
-  children: React.ReactElement
-}) => (
+export const AppProviders = ({ children }: { children: React.ReactNode }) => (
   <>
     <GlobalStyle />
     {children}

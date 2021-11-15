7ccfa08532f0f4cf375400a815dad342069c4eb6
tigerabrodi <tigerabrodi@gmail.com>
2021-11-15 08:50:53 +0100

    chore: remove comments

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -45,17 +45,4 @@ const registerUser = (user: User) => {
   }).should('exist')
 }
 
-// Cypress.Commands.add('login', (email, password) => { ... })
 Cypress.Commands.add('registerUser', registerUser)
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
